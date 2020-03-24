3b192b62b7b3cbe5e13136f9ac96ce7176641f9c
Nora van den Heuvel <noravdheuvel@gmail.com>
2020-03-24 15:11:36 +0000

    thermostat logic finished

--- src/thermostat.js
+++ src/thermostat.js
@@ -2,15 +2,32 @@
 
 class Thermostat{
   constructor() {
+    this.DEFAULT_TEMPERATURE = 20;
     this.MINIMUM_TEMPERATURE = 10; 
-    this.temperature = 20;
+    this.temperature = this.DEFAULT_TEMPERATURE;
     this.powerSavingMode = true;
+    this.MAX_LIMIT_PSM_ON = 25;
+    this.MAX_LIMIT_PSM_OFF = 32;
+    this.MEDIUM_ENERGY_USAGE_LIMIT = 18;
   }
   getCurrentTemperature() {
     return this.temperature;
   }
 
+  energyUsage() {
+    if(this.temperature < this.MEDIUM_ENERGY_USAGE_LIMIT){
+      return 'low-usage'
+    } else if (this.temperature > this.MAX_LIMIT_PSM_ON) {
+      return'high-usage'
+    } else {
+      return 'medium-usage'
+    }
+  }
+
   up(){
+    if(this.isMaximumTemperature()) {
+      return;
+    }
     this.temperature++
   }
 
@@ -21,9 +38,21 @@ class Thermostat{
     this.temperature--;
   }
 
+  resetTemperature() {
+    this.temperature = this.DEFAULT_TEMPERATURE;
+  }
+
   isMinimumTemperature() {
     return this.temperature === this.MINIMUM_TEMPERATURE;
   }
+
+  isMaximumTemperature() {
+    if (this.isPowerSavingModeOn() === false ) {
+      return this.temperature === this.MAX_LIMIT_PSM_OFF;
+    }
+    return this.temperature === this.MAX_LIMIT_PSM_ON;
+  }
+
   isPowerSavingModeOn() {
     return this.powerSavingMode === true;
   }
